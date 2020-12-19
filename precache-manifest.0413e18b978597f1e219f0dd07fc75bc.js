0b0095efdc6b291b8b9402b19fec04183abb79b9
Wang-XS <Wang-XS@hotmail.com>
2020-12-19 19:02:05 +0800

    deploy

---
+++ precache-manifest.0413e18b978597f1e219f0dd07fc75bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443467b66c47ee1e1db8",
+    "url": "/13-money-website/css/app.a2ff2ebf.css"
+  },
+  {
+    "revision": "72d1fad4547c802b35a8c80883ea8723",
+    "url": "/13-money-website/index.html"
+  },
+  {
+    "revision": "443467b66c47ee1e1db8",
+    "url": "/13-money-website/js/app.45bd3fde.js"
+  },
+  {
+    "revision": "241f91de4ee496d71f35",
+    "url": "/13-money-website/js/chunk-vendors.d36db51b.js"
+  },
+  {
+    "revision": "35ab4f9d23e53a35300df3750ff20186",
+    "url": "/13-money-website/manifest.json"
+  },
+  {
+    "revision": "738973740c0fec606727e6651e3878bc",
+    "url": "/13-money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/13-money-website/robots.txt"
+  }
+]);
